ecb048ae97ded4ff718d8417a3884e98a6edc211
Chen, You <you.chen@intel.com>
2023-08-15 22:10:35 +0800

    bind iaa plugin

---
+++ plugin/iaa-plugin-rocksdb/README.md
@@ -0,0 +1,133 @@
+# Intel&reg; In-Memory Analytics Accelerator Plugin for RocksDB Storage Engine
+The Intel&reg; In-Memory Analytics Accelerator (IAA) plugin for RocksDB provides accelerated compression/decompression in RocksDB using IAA and [QPL](https://github.com/intel/qpl) (Query Processing Library). It is dependent on the pluggable compression framework offered in [PR6717](https://github.com/facebook/rocksdb/pull/6717) and currently under review. Note: as the code is under review, it is subject to change. Please use the latest release of this plugin to ensure compatibility with the latest content of the pluggable compression PR.
+
+For more information about the Intel&reg; In-Memory Analytics Accelerator, refer to the [IAA spec](https://cdrdv2.intel.com/v1/dl/getContent/721858) on the [Intel&reg; 64 and IA-32 Architectures Software Developer Manuals](https://www.intel.com/content/www/us/en/developer/articles/technical/intel-sdm.html) page.
+For more information about plugin support in RocksDB, refer to the [instructions](https://github.com/facebook/rocksdb/tree/main/plugin) in RocksDB and [PR 7918](https://github.com/facebook/rocksdb/pull/7918).
+
+
+# Building RocksDB with the IAA Plugin
+
+- Install QPL. Follow the instructions in QPL's [readme](https://github.com/intel/qpl). The IAA plugin was tested with QPL [v1.1.0](https://github.com/intel/qpl/releases/tag/v1.1.0). Note that to access the hardware path and configure IAA, kernel 5.18 and accel-config are required, as described in QPL's [system requirements](https://intel.github.io/qpl/documentation/get_started_docs/system_requirements.html). The plugin requires shared workqueues to be configured with block_on_fault enabled.
+
+- Clone RocksDB with pluggable compression support, under review in [PR6717](https://github.com/facebook/rocksdb/pull/6717)
+
+```
+git clone --branch pluggable_compression https://github.com/lucagiac81/rocksdb.git
+cd rocksdb
+```
+
+- Clone the IAA plugin in the plugin directory in RocksDB
+
+```
+git clone https://github.com/intel/iaa-plugin-rocksdb.git plugin/iaa_compressor
+```
+
+### Build with make
+
+```
+ROCKSDB_PLUGINS="iaa_compressor" make -j release
+```
+
+If QPL was not installed in a default directory, add EXTRA_CXXFLAGS and EXTRA_LDFLAGS. For example
+
+```
+EXTRA_CXXFLAGS="-I<qpl_install_directory>/include" EXTRA_LDFLAGS="-L<qpl_install_directory>/lib64" ROCKSDB_PLUGINS="iaa_compressor" make -j release
+```
+
+### Build with CMake
+
+```
+mkdir build
+cd build
+cmake .. -DCMAKE_BUILD_TYPE=Release -DROCKSDB_PLUGINS="iaa_compressor"
+make -j
+```
+
+If QPL was not installed in a default directory, add CXXFLAGS and LDFLAGS in the cmake command
+
+```
+CXXFLAGS="-I/<qpl_install_directory>/include" LDFLAGS="-L<qpl_install_directory>lib64" cmake .. -DROCKSDB_PLUGINS="iaa_compressor"
+```
+
+### Verify Installation
+To verify the installation, you can use db_bench and verify no errors are reported. The first command below verifies the software path, the second the hardware path.
+
+```
+./db_bench --benchmarks=fillseq --compression_type=com.intel.iaa_compressor_rocksdb --compressor_options="execution_path=sw"
+./db_bench --benchmarks=fillseq --compression_type=com.intel.iaa_compressor_rocksdb --compressor_options="execution_path=hw"
+```
+
+# Testing
+
+- Install QPL, RocksDB with pluggable compression support, and IAA plugin as described in the previous section.
+- Install [googletest](https://github.com/google/googletest)
+- Build RocksDB as a shared library
+
+```
+LIB_MODE=shared make -j release
+```
+
+- Go to the tests directory of the IAA plugin and build the tests with CMake
+
+```
+cd plugin/iaa_compressor/tests
+mkdir build
+cd build
+cmake ..
+make run
+```
+
+If QPL and RocksDB were not installed in default directories, the path can be specified as follows
+
+```
+cmake -DROCKSDB_PATH=<rocksdb_install_directory> -DQPL_PATH=<qpl_install_directory> ..
+```
+
+To run only tests using the QPL software path (not using the IAA hardware), use the option -DEXCLUDE_HW_TESTS=ON.
+
+# Using the Plugin
+
+To use the IAA plugin for compression/decompression, select it as compression type (com.intel.iaa_compressor_rocksdb) just like any other algorithm. Refer to the examples in [PR6717](https://github.com/facebook/rocksdb/pull/6717). The reverse domain naming convention was selected to avoid conflicts in the future as more plugins are available. 
+
+In the following examples, execution_path is used as an example of compressor options. You can use any combination of supported options (refer to the Compressor Options section) in a semicolon-separated list.
+
+To configure RocksDB using an option string
+
+```
+compressor={id=com.intel.iaa_compressor_rocksdb;execution_path=hw}
+```
+
+To configure RocksDB using an Options object
+
+```
+Options options;
+ConfigOptions config_options;
+  Status s = Compressor::CreateFromString(
+      config_options,
+      "id=com.intel.iaa_compressor_rocksdb;execution_path=hw",
+      &options.compressor);
+```
+
+To select in db_bench
+
+```
+./db_bench --compression_type=com.intel.iaa_compressor_rocksdb  --compressor_options="execution_path=hw"
+```
+
+# Compressor Options
+
+The compressor offers several options:
+- execution_path
+  - "auto" (default): QPL decides whether to use the hardware or software path.
+  - "hw": QPL will use the IAA hardware.
+  - "sw": QPL will run compression/decompression in software.
+- compression_mode
+  - "dynamic" (default): for compression, a Huffman table is computed each time (requires two passes over the data, but provides, in general, better compression ratio).
+  - "fixed": a predefined Huffman table is used.
+- verify
+  - "true": run verification for compression (decompress and verify data matches original).
+  - "false" (default): skip verification.
+- level
+  - "0" or kDefaultCompressionLevel (default): default compression level (supported by hardware and software path).
+  - otherwise: high compression level (supported only by software path).
+- parallel_threads: refer to the parallel_threads option in RocksDB. Default = 1.
---
+++ plugin/iaa-plugin-rocksdb/iaa_compressor.cc
@@ -0,0 +1,314 @@
+// Copyright (C) 2022 Intel Corporation
+
+// SPDX-License-Identifier: Apache-2.0
+
+#include "iaa_compressor.h"
+
+#include <atomic>
+#include <cstddef>
+#include <cstdint>
+#include <functional>
+#include <list>
+#include <memory>
+#include <string>
+#include <vector>
+
+#include "logging/logging.h"
+#include "qpl/qpl.h"
+#include "rocksdb/compressor.h"
+#include "rocksdb/configurable.h"
+#include "rocksdb/env.h"
+#include "rocksdb/utilities/options_type.h"
+#include "util/coding.h"
+
+namespace ROCKSDB_NAMESPACE {
+
+// Error messages
+#define MEMORY_ALLOCATION_ERROR "memory allocation error"
+#define JOB_INIT_ERROR "job init error"
+#define QPL_STATUS(status) "QPL status " + std::to_string(status)
+
+extern "C" FactoryFunc<Compressor> iaa_compressor_reg;
+
+FactoryFunc<Compressor> iaa_compressor_reg =
+    ObjectLibrary::Default()->AddFactory<Compressor>(
+        "com.intel.iaa_compressor_rocksdb",
+        [](const std::string& /* uri */,
+           std::unique_ptr<Compressor>* compressor, std::string* /* errmsg */) {
+          *compressor = NewIAACompressor();
+          return compressor->get();
+        });
+
+std::unordered_map<std::string, qpl_path_t> execution_paths{
+    {"auto", qpl_path_auto},
+    {"hw", qpl_path_hardware},
+    {"sw", qpl_path_software}};
+
+enum qpl_compression_mode { dynamic_mode, fixed_mode };
+
+std::unordered_map<std::string, qpl_compression_mode> compression_modes{
+    {"dynamic", dynamic_mode}, {"fixed", fixed_mode}};
+
+struct IAACompressorOptions {
+  static const char* kName() { return "IAACompressorOptions"; };
+  qpl_path_t execution_path = qpl_path_auto;
+  qpl_compression_mode compression_mode = dynamic_mode;
+  bool verify = false;
+  int level = 0;
+  uint32_t parallel_threads = 1;
+};
+
+static std::unordered_map<std::string, OptionTypeInfo>
+    iaa_compressor_type_info = {
+        {"execution_path",
+         OptionTypeInfo::Enum(
+             offsetof(struct IAACompressorOptions, execution_path),
+             &execution_paths)},
+        {"compression_mode",
+         OptionTypeInfo::Enum(
+             offsetof(struct IAACompressorOptions, compression_mode),
+             &compression_modes)},
+        {"verify",
+         {offsetof(struct IAACompressorOptions, verify), OptionType::kBoolean,
+          OptionVerificationType::kNormal, OptionTypeFlags::kNone}},
+        {"level",
+         {offsetof(struct IAACompressorOptions, level), OptionType::kInt,
+          OptionVerificationType::kNormal, OptionTypeFlags::kNone}},
+        {"parallel_threads",
+         {offsetof(struct IAACompressorOptions, parallel_threads),
+          OptionType::kUInt32T, OptionVerificationType::kNormal,
+          OptionTypeFlags::kNone}}};
+
+class IAAJob {
+ public:
+  IAAJob() : jobs_(3, nullptr) {
+    InitJob(qpl_path_hardware);
+    InitJob(qpl_path_software);
+    InitJob(qpl_path_auto);
+  }
+
+  ~IAAJob() {
+    for (qpl_job* job : jobs_) {
+      if (job != nullptr) {
+        qpl_fini_job(job);
+        delete[] job;
+      }
+    }
+  }
+
+  qpl_job* GetJob(qpl_path_t execution_path) { return jobs_[execution_path]; }
+
+ private:
+  void InitJob(qpl_path_t execution_path) {
+    uint32_t size;
+    qpl_status status = qpl_get_job_size(execution_path, &size);
+    if (status != QPL_STS_OK) {
+      jobs_[execution_path] = nullptr;
+      return;
+    }
+    try {
+      jobs_[execution_path] = reinterpret_cast<qpl_job*>(new char[size]);
+    } catch (std::bad_alloc& e) {
+      jobs_[execution_path] = nullptr;
+      return;
+    }
+    status = qpl_init_job(execution_path, jobs_[execution_path]);
+    if (status != QPL_STS_OK) {
+      jobs_[execution_path] = nullptr;
+    }
+  }
+
+  std::vector<qpl_job*> jobs_;
+};
+
+class IAACompressor : public Compressor {
+ public:
+  IAACompressor() {
+    RegisterOptions(&options_, &iaa_compressor_type_info);
+
+#ifndef NDEBUG
+    Status s =
+        Env::Default()->NewLogger("/tmp/iaa_compressor_log.txt", &logger_);
+    if (s.ok()) {
+      logger_->SetInfoLogLevel(DEBUG_LEVEL);
+    }
+#endif
+  };
+
+  static const char* kClassName() { return "com.intel.iaa_compressor_rocksdb"; }
+
+  const char* Name() const override { return kClassName(); }
+
+  bool DictCompressionSupported() const override { return false; }
+
+  uint32_t GetParallelThreads() const override {
+    return options_.parallel_threads;
+  };
+
+  Status Compress(const CompressionInfo& /* info */, const Slice& input,
+                  std::string* output) override {
+    // Max size of a RocksDB block is 4GiB
+    uint32_t output_header_length = EncodeSize(input.size(), output);
+
+    // If data is incompressible, QPL returns stored blocks
+    // A stored block is at most 2^16-1 bytes in size and it has a 5-byte header
+    // So, in the worst case, data grows by 5*ceil(input.size()/65535)
+    size_t input_length = input.size();
+    size_t output_length =
+        output_header_length + input_length +
+        (input_length / 65535 + (input_length % 65535 != 0)) * 5;
+    if (output_length > std::numeric_limits<uint32_t>::max()) {
+      // Attempt compression with largest possible buffer. QPL will return an
+      // error if not sufficient.
+      output_length = std::numeric_limits<uint32_t>::max();
+    }
+    output->resize(output_length);
+
+    qpl_status status;
+    qpl_compression_levels level = GetQplLevel(options_.level);
+    qpl_path_t execution_path = options_.execution_path;
+
+    if (level == qpl_high_level && execution_path == qpl_path_hardware) {
+      execution_path = qpl_path_software;
+    }
+
+    qpl_job* job = job_.GetJob(execution_path);
+    if (job == nullptr) {
+      return Status::Corruption(JOB_INIT_ERROR);
+    }
+
+    uint8_t* source =
+        const_cast<uint8_t*>(reinterpret_cast<const uint8_t*>(input.data()));
+    uint8_t* destination =
+        reinterpret_cast<uint8_t*>(&(*output)[0] + output_header_length);
+
+    job->next_in_ptr = source;
+    job->available_in = input.size();
+    job->next_out_ptr = destination;
+    job->available_out = output_length - output_header_length;
+    job->level = level;
+    job->op = qpl_op_compress;
+    job->flags = QPL_FLAG_FIRST | QPL_FLAG_LAST;
+    if (!options_.verify) {
+      job->flags |= QPL_FLAG_OMIT_VERIFY;
+    }
+    job->huffman_table = nullptr;
+    job->dictionary = nullptr;
+
+    if (options_.compression_mode == dynamic_mode) {
+      job->flags |= QPL_FLAG_DYNAMIC_HUFFMAN;
+    }
+
+    status = QPL_STS_QUEUES_ARE_BUSY_ERR;
+    while (status == QPL_STS_QUEUES_ARE_BUSY_ERR) {
+      status = qpl_execute_job(job);
+    }
+
+    if (status != QPL_STS_OK) {
+      return Status::Corruption(QPL_STATUS(status));
+    }
+    output->resize(output_header_length + job->total_out);
+    Debug(logger_, "Compress - input size: %lu - output size: %u\n",
+          input.size(), job->total_out);
+
+    return Status::OK();
+  }
+
+  Status Uncompress(const UncompressionInfo& info, const char* input,
+                    size_t input_length, char** output,
+                    size_t* output_length) override {
+    // Extract uncompressed size
+    uint32_t encoded_output_length = 0;
+    if (!DecodeSize(&input, &input_length, &encoded_output_length)) {
+      return Status::Corruption("size decoding error");
+    }
+
+    // Memory allocator may return null pointer or throw bad_alloc exception
+    try {
+      *output = Allocate(encoded_output_length, info.GetMemoryAllocator());
+      if (*output == nullptr) {
+        return Status::Corruption(MEMORY_ALLOCATION_ERROR);
+      }
+    } catch (std::bad_alloc& e) {
+      return Status::Corruption(MEMORY_ALLOCATION_ERROR);
+    }
+
+    qpl_status status;
+    qpl_job* job = job_.GetJob(options_.execution_path);
+    if (job == nullptr) {
+      return Status::Corruption(JOB_INIT_ERROR);
+    }
+
+    uint8_t* source =
+        const_cast<uint8_t*>(reinterpret_cast<const uint8_t*>(input));
+    uint8_t* destination = reinterpret_cast<uint8_t*>(*output);
+
+    job->next_in_ptr = source;
+    job->available_in = input_length;
+    job->next_out_ptr = destination;
+    job->available_out = encoded_output_length;
+    job->op = qpl_op_decompress;
+    job->huffman_table = nullptr;
+    job->flags = QPL_FLAG_FIRST | QPL_FLAG_LAST;
+
+    status = QPL_STS_QUEUES_ARE_BUSY_ERR;
+    while (status == QPL_STS_QUEUES_ARE_BUSY_ERR) {
+      status = qpl_execute_job(job);
+    }
+
+    if (status != QPL_STS_OK) {
+      return Status::Corruption(QPL_STATUS(status));
+    } else if (job->total_out != encoded_output_length) {
+      return Status::Corruption("size mismatch");
+    }
+    *output_length = job->total_out;
+    Debug(logger_, "Uncompress - input size: %lu - output size: %u\n",
+          input_length, job->total_out);
+
+    return Status::OK();
+  }
+
+  bool IsDictEnabled() const override { return false; }
+
+ private:
+  IAACompressorOptions options_;
+  static thread_local IAAJob job_;
+  std::shared_ptr<Logger> logger_;
+
+  uint32_t EncodeSize(size_t length, std::string* output) {
+    PutVarint32(output, length);
+    return output->size();
+  }
+
+  bool DecodeSize(const char** input, size_t* input_length,
+                  uint32_t* output_length) {
+    auto new_input =
+        GetVarint32Ptr(*input, *input + *input_length, output_length);
+    if (new_input == nullptr) {
+      return false;
+    }
+    *input_length -= (new_input - *input);
+    *input = new_input;
+    return true;
+  }
+
+  int GetLevel() const override { return options_.level; }
+
+  qpl_compression_levels GetQplLevel(int level) {
+    if (level == 0 || level == CompressionOptions::kDefaultCompressionLevel) {
+      return qpl_default_level;
+    } else {
+      return qpl_high_level;
+    }
+  }
+};
+
+// Reuse job structs across calls. Have one struct per thread and execution path
+// (hw, sw, auto).
+thread_local IAAJob IAACompressor::job_;
+
+std::unique_ptr<Compressor> NewIAACompressor() {
+  return std::unique_ptr<Compressor>(new IAACompressor());
+}
+
+}  // namespace ROCKSDB_NAMESPACE
